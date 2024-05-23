c5a379d7169a423a20b274ab4267dffa6b3c434d
apaselina <kovpool@icloud.com>
2024-05-23 15:08:16 +0300

    add

--- app/api/config.js
+++ app/api/config.js
@@ -1,4 +1,4 @@
-export const BASE_URL = 'http://apollo.nomoredomainswork.ru/api'
+export const BASE_URL = 'https://apollo.nomoredomainswork.ru/api'
 
 export const endpoints = {
   games: `${BASE_URL}/games`,
